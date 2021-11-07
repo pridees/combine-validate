2aa9c56425b9f830d238dbc12a880d605b8c81a2
Alex Ovcharenko <aleksandr.ovcharenko@irlix.ru>
2021-11-07 22:41:29 +0300

    Clean up & refactor

--- Sources/CombineValidate/Views/View+Validate.swift
+++ Sources/CombineValidate/Views/View+Validate.swift
@@ -4,16 +4,14 @@ import SwiftUI
 extension TextField {
     public func validate(
         for publisher: ValidationPublisher,
-        configuration: ValidationWrapper<Self, Void>.Configuration = .default,
-        _ onValidate: ((Validated<Void>) -> Void)? = nil
+        configuration: ValidationWrapper<Self, Void>.Configuration = .default
     ) -> some View {
         ValidationWrapper(self, publisher, configuration: configuration)
     }
     
     public func validate<T: RegexProtocol>(
         for publisher: RichValidationPublisher<T>,
-        configuration: ValidationWrapper<Self, T>.Configuration = .default,
-        _ onValidate: ((Validated<T>) -> Void)? = nil
+        configuration: ValidationWrapper<Self, T>.Configuration = .default
     ) -> some View {
         ValidationWrapper(self, publisher, configuration: configuration)
     }
@@ -22,8 +20,7 @@ extension TextField {
 extension SecureField {
     public func validate(
         for publisher: ValidationPublisher,
-        configuration: ValidationWrapper<Self, Void>.Configuration = .default,
-        _ onValidate: ((Validated<Void>) -> Void)? = nil
+        configuration: ValidationWrapper<Self, Void>.Configuration = .default
     ) -> some View {
         ValidationWrapper(self, publisher, configuration: configuration)
     }
@@ -31,8 +28,7 @@ extension SecureField {
 
 extension Toggle {
     public func validate(
-        for publisher: ValidationPublisher,
-        _ onValidate: ((Validated<Void>) -> Void)? = nil
+        for publisher: ValidationPublisher
     ) -> some View {
         ValidationWrapper(self, publisher, configuration: .hintOnly)
     }
