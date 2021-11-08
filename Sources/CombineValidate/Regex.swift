a670f07d6c01edc0c13537e24bea2c5870c7606b
Alex O <pridees@gmail.com>
2021-11-08 15:19:40 +0300

    Add more regexps

--- Sources/CombineValidate/Regex.swift
+++ Sources/CombineValidate/Regex.swift
@@ -8,6 +8,10 @@ public protocol RegexProtocol where Self: RawRepresentable, Self.RawValue == Reg
     var pattern: RegexPattern { get }
 }
 
+public extension RegexProtocol {
+    var pattern: RegexPattern { self.rawValue }
+}
+
 /// Extensions with predefined popular regular expressions
 ///
 /// Conforming to ``RegexPattern`` & ``RegexProtocol``
@@ -23,9 +27,17 @@ public enum RegularPattern: RegexPattern, RegexProtocol {
     /// - 1 number
     case strongPassword = #"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"#
     
+    case notEmpty = #"[\S\s]+[\S]+"#
+    
+    case mustIncludeCapitalLetters = #"(?=.*[A-Z])"#
+    
+    case mustIncludeSpecialSymbols = #"(?=.*[\%\!-\/\:-\@\[-\`\{-\~])"#
+    
+    case mustIncludeNumbers = #"(?=.*[0-9])"#
+    
+    case mustIncludeSmallLetters = #"(?=.*[a-z])"#
+    
     case wordAndDigitsOnly = #"^(\d|\w|\S){1,}$"#
     
     case anyInOneLine = #"^.+$"#
-    
-    public var pattern: RegexPattern { self.rawValue }
 }
